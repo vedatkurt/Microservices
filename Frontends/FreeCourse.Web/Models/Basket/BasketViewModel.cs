1391970c734df66025a30bc8df8c605ad8b5b8a8
jumpkurt <vekurt@gmail.com>
2021-12-29 15:36:33 +0300

    Commit All

--- Frontends/FreeCourse.Web/Models/Basket/BasketViewModel.cs
+++ Frontends/FreeCourse.Web/Models/Basket/BasketViewModel.cs
@@ -7,10 +7,15 @@ namespace FreeCourse.Web.Models.Basket
 {
     public class BasketViewModel
     {
+        public BasketViewModel()
+        {
+            _basketItems = new List<BasketItemViewModel>();
+        }
+
         public string UserId { get; set; }
         public string DiscountCode { get; set; }
         public int? DiscountRate { get; set; } = 10;
-        private List<BasketItemViewModel> _basketItems { get; set; }
+        private List<BasketItemViewModel> _basketItems;
 
         public List<BasketItemViewModel> BasketItems {
             get
